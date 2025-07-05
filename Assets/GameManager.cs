3c2f6823c6865f7d1a974333daf9ced5eeb40fc3
Terra <106891646+TerraInCaelo@users.noreply.github.com>
2025-07-05 10:56:27 +0200

    wafawfe

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour{
     [SerializeField] private string[] levelNames;
     [SerializeField] private GameObject[] playerReferences = new GameObject[2];
+    [SerializeField] private TMP_Text[] scoreVisualisation; 
     private static GameManager instance;
     private int[] score = new int[2];
     private void Awake(){
@@ -15,15 +17,22 @@ public class GameManager : MonoBehaviour{
     }
     public static void PlayerDied(byte id){
         instance.score[id]++;
+        instance.scoreVisualisation[id].text = $"{instance.score[id]}";
         NextMap();
     }
     private static void NextMap(){
         SceneManager.LoadScene(instance.levelNames[Random.Range(0, instance.levelNames.Length)]);
         ResurrectPlayers();
+        MovePlayersToSpawnpoints();  
     }
     private static void ResurrectPlayers(){
         foreach (var player in instance.playerReferences){
             player.GetComponent<IHealth>().Resurrect();
+        }
+    }
+    private static void MovePlayersToSpawnpoints(){
+        for(byte i = 0; i < instance.playerReferences.Length; i++){
+            instance.playerReferences[i].transform.position = Spawnpoints.GetSpawnpoints(i);
         }
     }
 }

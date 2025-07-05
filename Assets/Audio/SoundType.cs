4a930c223a7ba7ec883ae86a56f7f5873eeae760
Albert Przytuła <przytulaalbert@gmail.com>
2025-07-05 18:18:41 +0200

    audio radyjko glosniczek

---
+++ Assets/Audio/SoundType.cs
@@ -0,0 +1,19 @@
+
+public enum SoundType
+{
+    DOTYK_MIDASA,
+    TRZECIA_W_NOCY,
+    NEURONY_PIXELE,
+    HIT,
+    HIT_SMALL,
+    JUMP,
+    POWER_UP_1,
+    POWER_UP_2,
+    RANDOM,
+    SAWMILL,
+    SHOOTGUN,
+    SLOWMO_START,
+    SLOWMO_END,
+    SNIPER,
+    STEP2,
+}

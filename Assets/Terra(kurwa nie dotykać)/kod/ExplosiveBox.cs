bf9a79a80eacb9ffb77b0147ae3b95f9cef121c2
Malastin <malastin2004@gmail.com>
2025-07-06 01:24:34 +0200

    dick

--- Assets/Terra(kurwa nie dotykać)/kod/ExplosiveBox.cs
+++ Assets/Terra(kurwa nie dotykać)/kod/ExplosiveBox.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
 
 public class ExplosiveBox : Box{
-    [SerializeField] private GameObject explosion;
 
     protected override void Death(){
         GetComponent<DamageEffect>().TriggerDamageSplash();
         Debug.Log("BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM");
-
-        var explosionObj = Instantiate(explosion, transform.parent);
-        explosion.transform.position = transform.position;
-        explosion.transform.localScale = new Vector3(1.5f, 1.5f, 1);
         Destroy(gameObject);
     }
 }

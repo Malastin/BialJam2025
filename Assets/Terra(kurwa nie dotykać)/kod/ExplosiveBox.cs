707784db5488f189286ea3e46adf10018be22c12
Malastin <malastin2004@gmail.com>
2025-07-06 11:16:08 +0200

    gay

--- Assets/Terra(kurwa nie dotykać)/kod/ExplosiveBox.cs
+++ Assets/Terra(kurwa nie dotykać)/kod/ExplosiveBox.cs
@@ -4,6 +4,7 @@ public class ExplosiveBox : Box{
 
     [SerializeField] private GameObject explosionObj;
     [SerializeField] private GameObject particles;
+    [SerializeField] private GameObject particles2;
 
     protected override void Death(){
         GetComponent<DamageEffect>().TriggerDamageSplash();
@@ -15,5 +16,7 @@ public class ExplosiveBox : Box{
         spawnExplosion.transform.localScale = new Vector3(3, 3, 1);
         var spawnParticles = Instantiate(particles, transform.parent);
         spawnParticles.transform.position = transform.position;
+        var spawnParticles2 = Instantiate(particles2, transform.parent);
+        spawnParticles2.transform.position = transform.position;
     }
 }
